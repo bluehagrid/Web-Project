9d5203a5b361292d14dd7887486f2fc9ebaab0db
Bluehagrid <jacob.jackson@dixiesuccess.org>
2019-11-04 14:50:15 -0700

    Added radio buttons

--- my-web-project/src/app/mock-users.ts
+++ my-web-project/src/app/mock-users.ts
@@ -1,14 +1,14 @@
 import { User } from './user';
 
 export const USERS: User[] = [
-  { id: 11, name: 'Dr Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 1, name: 'Question 1' },
+  { id: 2, name: 'Question 2' },
+  { id: 3, name: 'Question 3' },
+  { id: 4, name: 'Question 4' },
+  { id: 5, name: 'Question 5' },
+  { id: 6, name: 'Question 6' },
+  { id: 7, name: 'Question 7' },
+  { id: 8, name: 'Question 8' },
+  { id: 9, name: 'Question 9' },
+  { id: 10, name: 'Question 10' }
 ];
